52c62ae23e58c5005056a0b30d39f1529cb0b9fe
prasad <prasadpatil2309@gmail.com>
2024-08-22 00:47:16 +0530

    boiler plate code spring boot

---
+++ src/main/java/com/datazipper/DatazipperApplication.java
@@ -0,0 +1,13 @@
+package com.datazipper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatazipperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatazipperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datazipper/DatazipperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datazipper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatazipperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
